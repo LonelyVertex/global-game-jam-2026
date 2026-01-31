b3b34a164e7a31ac50dcde3d75599ffda0ad2762
Ondrej Kofron <kofron.ondrej@gmail.com>
2026-01-31 09:29:17 +0100

    Dash

--- Assets/InputSystem_Actions.cs
+++ Assets/InputSystem_Actions.cs
@@ -114,7 +114,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Attack"",
                     ""type"": ""Button"",
                     ""id"": ""6c2ab1b8-8984-453a-af3d-a3c78ae1679a"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -141,7 +141,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Jump"",
                     ""type"": ""Button"",
                     ""id"": ""f1ba0d36-48eb-4cd5-b651-1c94a6531f70"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -169,6 +169,15 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""type"": ""Button"",
                     ""id"": ""641cd816-40e6-41b4-8c3d-04687c349290"",
                     ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""bd11fa6d-7780-4bc8-85de-d399c6688f19"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -557,6 +566,28 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
                     ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0b901c99-a1ff-400b-8170-8b5e760e69c1"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Keyboard&Mouse"",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c8f8459d-af26-46ae-bc98-08b623a755e9"",
+                    ""path"": ""<Gamepad>/leftTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Gamepad"",
+                    ""action"": ""Dash"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1153,6 +1184,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_Player_Previous = m_Player.FindAction("Previous", throwIfNotFound: true);
         m_Player_Next = m_Player.FindAction("Next", throwIfNotFound: true);
         m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1255,6 +1287,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Previous;
     private readonly InputAction m_Player_Next;
     private readonly InputAction m_Player_Sprint;
+    private readonly InputAction m_Player_Dash;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -1302,6 +1335,10 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Sprint".
         /// </summary>
         public InputAction @Sprint => m_Wrapper.m_Player_Sprint;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Dash".
+        /// </summary>
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1355,6 +1392,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Sprint.started += instance.OnSprint;
             @Sprint.performed += instance.OnSprint;
             @Sprint.canceled += instance.OnSprint;
+            @Dash.started += instance.OnDash;
+            @Dash.performed += instance.OnDash;
+            @Dash.canceled += instance.OnDash;
         }
 
         /// <summary>
@@ -1393,6 +1433,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Sprint.started -= instance.OnSprint;
             @Sprint.performed -= instance.OnSprint;
             @Sprint.canceled -= instance.OnSprint;
+            @Dash.started -= instance.OnDash;
+            @Dash.performed -= instance.OnDash;
+            @Dash.canceled -= instance.OnDash;
         }
 
         /// <summary>
@@ -1756,6 +1799,13 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnSprint(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Dash" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnDash(InputAction.CallbackContext context);
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "UI" which allows adding and removing callbacks.
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,21 @@
 using UnityEngine;  
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public InputActionReference moveAction;
+    public InputActionReference dashAction;
     public Rigidbody rigidBody;
     public float moveSpeed = 5f;
     public Transform playerModel;
+    public float dashDistance = 5f;
+    public float dashCooldown = 2f;
+    public float dashSpeed = 20f;
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float lastDashTime = -Mathf.Infinity;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,13 +27,21 @@ public class PlayerController : MonoBehaviour
         else
         {
             moveAction.action.Enable();
+        }
+        if (dashAction == null)
+        {
+            Debug.LogError("Dash Action Reference is not assigned in the Inspector.");
+        }
+        else
+        {
+            dashAction.action.Enable();
         }      
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (moveAction != null && moveAction.action.enabled)
+        if (moveAction != null && moveAction.action.enabled && !isDashing)
         {
             Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
             Vector3 moveVector = new Vector3(moveInput.x, 0, moveInput.y);
@@ -37,6 +54,50 @@ public class PlayerController : MonoBehaviour
                 playerModel.rotation = Quaternion.Slerp(playerModel.rotation, toRotation, 15f * Time.fixedDeltaTime);
             }
         }
+    }
+
+    void Update() {
+        if (dashAction != null && dashAction.action.enabled && rigidBody != null && dashAction.action.WasPressedThisFrame())
+        {
+            Dash();
+        }
+    }
+
+
+    public void Dash()
+    {
+        Debug.Log("Dash");
+        if (canDash && Time.time >= lastDashTime + dashCooldown)
+        {
+            Vector3 dashDirection = playerModel.forward;
+            isDashing = true;
+            canDash = false;
+            lastDashTime = Time.time;
+
+            Vector3 dashTarget = rigidBody.position + dashDirection * dashDistance;
+            StartCoroutine(PerformDash(dashTarget));
+        }
+    }
+
+    IEnumerator PerformDash(Vector3 dashTarget)
+    {
+        float startTime = Time.time;
+        Vector3 startPosition = rigidBody.position;
+        float dashDuration = dashDistance / dashSpeed;
+
+        while (Time.time < startTime + dashDuration)
+        {
+            float t = (Time.time - startTime) / dashDuration;
+            rigidBody.MovePosition(Vector3.Lerp(startPosition, dashTarget, t));
+            yield return null;
+        }
+
+        rigidBody.MovePosition(dashTarget);
+        isDashing = false;
+
+        // Start cooldown timer
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 
 }
