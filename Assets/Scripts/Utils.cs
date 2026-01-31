ace4551f1ef150dd9c49a0cdf144a1e8f641d7b9
Jan Slifka <jan.slifka@gmail.com>
2026-01-31 14:31:38 +0100

    Add LoS check

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -8,7 +8,8 @@ public static class Utils
     {
         return Physics.OverlapSphere(position, range, LayerMask.GetMask("Enemy"))
             .Select(collider => collider.transform)
-            .Distinct();
+            .Distinct()
+            .Where(t => IsInLoS(position, t.position));
     }
 
     public static IEnumerable<Transform> FindEnemiesInRangeSorted(Vector3 position, float range)
@@ -16,6 +17,15 @@ public static class Utils
         return FindEnemiesInRange(position, range)
             .OrderBy(e => Vector3.Distance(position, e.position));
 
+    }
+
+    private static bool IsInLoS(Vector3 targetPosition, Vector3 sourcePosition)
+    {
+        var direction = targetPosition - sourcePosition;
+        float distance = Vector3.Distance(targetPosition, sourcePosition);
+        int layerMask = LayerMask.GetMask("Obstacles");
+
+        return !Physics.Raycast(sourcePosition, direction, distance, layerMask);
     }
 
     public static IEnumerable<T> Shuffle<T>(IEnumerable<T> source)
