297f58b0e0e85796019445db557661c922b67769
Jakub Mirovský <mirovsky.jakub@gmail.com>
2026-02-01 15:39:40 +0100

    Try navmesh optimization

--- Assets/Scripts/EnemyMovementController.cs
+++ Assets/Scripts/EnemyMovementController.cs
@@ -4,8 +4,13 @@ using UnityEngine.AI;
 public class EnemyMovementController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float updateDelay;
 
     private Transform _player;
+
+    private bool _hasPosition;
+    private Vector3 _lastPlayerPosition;
+    private float _lastPositionUpdate;
 
     void Start()
     {
@@ -16,6 +21,29 @@ public class EnemyMovementController : MonoBehaviour
     {
         if (!_player) return;
 
-        agent.SetDestination(_player.position);
+        float distanceToPlayer = Vector3.Distance(_player.position, transform.position);
+        if (distanceToPlayer > 10.0f)
+        {
+            agent.obstacleAvoidanceType = ObstacleAvoidanceType.LowQualityObstacleAvoidance;
+        }
+        else
+        {
+            agent.obstacleAvoidanceType = ObstacleAvoidanceType.MedQualityObstacleAvoidance;
+        }
+
+        float distanceFromLastSetPosition = Vector3.Distance(_player.position, _lastPlayerPosition);
+
+        if (!_hasPosition ||
+            distanceFromLastSetPosition > 10.0f ||
+            distanceToPlayer < 10.0f ||
+            Time.time - _lastPositionUpdate > updateDelay
+        )
+        {
+            _hasPosition = true;
+            _lastPlayerPosition = _player.position;
+            _lastPositionUpdate = Time.time;
+
+            agent.SetDestination(_player.position);
+        }
     }
 }
